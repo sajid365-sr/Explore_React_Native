ba070608f8ac553e7d2bd6cd1d750bada9cf49f5
sajid365-sr <sajidsorker86@gmail.com>
2023-02-06 01:33:57 +0600

    set up basic difference bitween react and react native using view and text

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text style={styles.red} >Hi, I'm sajid sorker. This is first React Native app</Text>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-web';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    );
+};
+
+export default Home;
